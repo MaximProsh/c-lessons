d24ded644986d3534ecf5e22f560da939808cecf
MaximProsh <karmark@yandex.ru>
2022-08-17 12:52:04 +0300

    Add IF ElSE program

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+    Console.WriteLine("ЭТО ЖЕ МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
